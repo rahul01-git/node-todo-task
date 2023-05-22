628e508f7b9ecd69cc98df26c2b05805492f16ca
rahul01-git <rahulneupane0090@gmail.com>
2023-05-22 13:18:10 +0545

    routes manages

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const getConnection = require('./config/db');
+const todoRoutes = require('./routes/todo.routes.');
 
 //init
 dotenv.config();
@@ -17,50 +18,9 @@ app.use((req, res, next) => {
     req.conn = conn;
     next();
 });
+
 //routes
-app.get("/", (req, res) => {
-    req.conn.query("SELECT * FROM todo", (error, result) => {
-        if (error) {
-            res.status(500).send("Error aayo")
-        }
-        res.render("index", { items: result.rows });
-    })
-
-
-})
-
-app.post("/add-todo", (req, res) => {
-    if (req.body.todo == '') res.redirect("/")
-    req.conn.query("insert into todo (title) values ($1)", [req.body.todo], (error, result) => {
-        if (error) {
-            res.status(500).send("Error aayo  hai");
-        }
-        res.redirect('/');
-    })
-})
-app.post("/completed-todo", (req, res) => {
-    const id = parseInt(req.body.id);
-    req.conn.query("select * from todo where id=$1", [id], (error, result) => {
-        if (error) {
-            res.sendStatus(500).send("error aayo")
-        }
-        req.conn.query("UPDATE todo SET iscomplete=$1 where id=$2", [!result.rows[0].iscomplete, id], (error, result) => {
-            if (error) {
-                res.sendStatus(500).send("error aayo")
-            }
-            res.redirect('/');
-        })
-    })
-})
-app.post("/remove-todo", (req, res) => {
-    const id = parseInt(req.body.id);
-    req.conn.query("delete from todo where id=$1", [id], (error, result) => {
-        if (error) {
-            res.sendStatus(500).send("error aayo")
-        }
-        res.redirect('/');
-    })
-})
+app.use('/',todoRoutes);
 
 //server activation
 app.listen(APP_PORT, () => {
